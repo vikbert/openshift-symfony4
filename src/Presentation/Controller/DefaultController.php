8a2f4afbe8ece677c525ae12a6fa167d36fd4105
Xun Zhou <xun.zhou@lidl.com>
2019-04-08 14:01:52 +0200

    dump the symfony version

--- src/Presentation/Controller/DefaultController.php
+++ src/Presentation/Controller/DefaultController.php
@@ -5,11 +5,12 @@ declare(strict_types = 1);
 namespace App\Presentation\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Kernel;
 
 final class DefaultController
 {
     public function index()
     {
-        return new Response('<h1>It works</h1><p>A Symfony 4 project deployed on OpenShift.</p>', 200);
+        return new Response(sprintf('<h1>Welcome to Symfony %s</h1>', Kernel::VERSION), 200);
     }
 }
